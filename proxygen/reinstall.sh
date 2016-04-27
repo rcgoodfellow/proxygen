b1bd19eb5165e1bc9b4d9afd1b9a2cf577f33f85
Ryan Goodfellow <rgoodfel@isi.edu>
2016-04-26 19:33:20 -0700

    grrrr

--- proxygen/reinstall.sh
+++ proxygen/reinstall.sh
@@ -4,6 +4,11 @@
 ## and folly). You must first compile all the dependencies before running this. This
 ## Usually this is done by first running `deps.sh`.
 
+export CXX=clang++
+export CC=clang
+export CXXFLAGS="-std=c++14 -stdlib=libc++ -I/usr/include/c++/v1 -Wno-sign-compare -Wno-reserved-user-defined-literal"
+export LD_LIBRARY_PATH=/usr/local/lib
+
 set -e
 start_dir=`pwd`
 trap "cd $start_dir" EXIT
